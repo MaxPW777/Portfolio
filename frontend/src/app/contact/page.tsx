6f691110c143852b77861c11d4028ef22b194752
Maximilian <mpinderwhite@gmail.com>
2024-07-12 12:17:05 +0200

    [+] contact form css

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -3,15 +3,10 @@ import ContactList from "@/components/contact/ContactList";
 
 function ContactPage() {
     return (
-        <div>
-            <h1>Contact</h1>
             <div className="flex abs-center">
-                <div className={'border rounded p-2'}>
-                    <ContactForm/>
-                </div>
+                <ContactForm/>
                 <ContactList/>
             </div>
-        </div>
     );
 }
 
--- frontend/src/components/contact/ContactForm.tsx
+++ frontend/src/components/contact/ContactForm.tsx
@@ -2,24 +2,47 @@
 import {useForm} from "react-hook-form";
 import {IContactRequestDto} from "@common/dto/IContactRequestDto";
 import {useCreateContactMutation} from "@/services/contact";
+import './contact-form.css';
 
 function ContactForm() {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const mutation = useCreateContactMutation();
-    const {register, handleSubmit, formState: {errors}} = useForm<IContactRequestDto>();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<IContactRequestDto>();
 
     const onSubmit = (data: IContactRequestDto) => {
         mutation.mutate(data);
     };
 
     return (
-        <form className={'flex flex-col'} onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Name" {...register('name', {required: true})}/>
-            {errors.name && <span>Name is required</span>}
-            <input type="email" placeholder="Email" {...register('email', {required: true})}/>
-            {errors.email && <span>Email is required</span>}
-            <textarea placeholder="Message" {...register('message', {required: true})}/>
-            {errors.message && <span>Message is required</span>}
-            <button type="submit">Submit</button>
+        <form className={'contact'} onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="text-xl mb-4">Contact Me</h2>
+            <label className={'contact-label'}>Name
+                <input className={'contact-input'} type="text"
+                       placeholder="Name" {...register('name', {required: true})}/>
+                {errors.name && <span>Name is required</span>}
+            </label>
+            <label className={'contact-label'}>Email
+                <input className={'contact-input'} type="email"
+                       placeholder="Email" {...register('email', {
+                    required: true,
+                    pattern: emailRegex
+                })}/>
+                {errors.email && <span>Please enter a valid email address</span>}
+            </label>
+            <label className={'contact-label'}>Message
+                <textarea
+                    className={'w-full border border-gray-300 rounded-lg p-2'}
+                    placeholder="Message" {...register('message', {required: true})}/>
+                {errors.message && <span>Message is required</span>}
+            </label>
+            <button
+                className={'bg-blue-500 text-white rounded-lg p-2 hover:bg-blue-700'}
+                type="submit">Submit
+            </button>
         </form>
     )
 }
