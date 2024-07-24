51f54053f1d5b0e3b32b422ec85c5caeaf7d97bf
Maximilian <mpinderwhite@gmail.com>
2024-07-24 10:38:34 +0200

    [~] redesigned login component

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -1,6 +1,16 @@
 "use client";
 import {useAuth} from '@/providers/auth-context';
 import {SubmitHandler, useForm} from "react-hook-form";
+import {Card, CardHeader, CardTitle} from "@/components/ui/card";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel, FormMessage
+} from '@/components/ui/form';
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
 
 type LoginInputs = {
     username: string,
@@ -8,10 +18,7 @@ type LoginInputs = {
 }
 
 function LoginForm() {
-    const {
-        register, handleSubmit,
-        formState: {errors}
-    } = useForm<LoginInputs>()
+    const form = useForm<LoginInputs>()
     const {login} = useAuth();
 
     const onSubmit: SubmitHandler<LoginInputs> = (data) => loginUser(data)
@@ -28,33 +35,42 @@ function LoginForm() {
     };
 
     return (
-        <div
-            className="abs-center flex flex-col items-center justify-center text-primary bg-[rgba(0,0,0,0.2)] rounded">
-            <form onSubmit={handleSubmit(onSubmit)}
-                  className="flex flex-col gap-4 mx-auto p-6 rounded-lg">
-                <h2 className="text-xl mb-4">Login</h2>
-                {errors.username && <span>Please enter your username</span>}
-                <label className="flex flex-col text-sm">
-                    Username:
-                    <input
-                        {...register("username", {required: true})}
-                        className="mt-1 p-2 bg-gray-700 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        type="text" name="username" required/>
-                </label>
-                {errors.password && <span>Please enter your password</span>}
-                <label className="flex flex-col text-sm">
-                    Password:
-                    <input
-                        {...register("password", {required: true})}
-                        className="mt-1 p-2 bg-gray-700 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        type="password" name="password" required/>
-                </label>
-                <button
-                    className="mt-4 p-2 bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    type="submit">Login
-                </button>
-            </form>
-        </div>
+        <Card className={'abs-center p-4 '}>
+            <CardHeader>
+                <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <Form {...form}>
+                <form className={'space-y-4'} onSubmit={form.handleSubmit(loginUser)}>
+                    <FormField
+                        control={form.control}
+                        name={'username'}
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder={'username'} {...field}></Input>
+                                </FormControl>
+                            <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name={'password'}
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder={'password'} {...field}></Input>
+                                </FormControl>
+                            <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Login</Button>
+                </form>
+            </Form>
+        </Card>
     );
 }
 
