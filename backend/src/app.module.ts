53f71bbf54904a9be7fb16044c8573fd1afacfd0
Maximilian <mpinderwhite@gmail.com>
2024-07-29 10:50:17 +0200

    [+] added correct imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,11 +8,12 @@ import { AuthModule } from './auth/auth.module';
 import { CommentModule } from './comment/comment.module';
 import { ContactModule } from './contact/contact.module';
 import {S3Module} from "@/s3/s3.module";
+import {ExperienceModule} from "@/experience/experience.module";
 
 require('dotenv').config();
 
 @Module({
-    imports: [MongooseModule.forRoot(process.env.MONGO_URI), UserModule, PostModule, AuthModule, CommentModule, ContactModule, S3Module],
+    imports: [MongooseModule.forRoot(process.env.MONGO_URI), UserModule, PostModule, ExperienceModule, AuthModule, CommentModule, ContactModule, S3Module],
     controllers: [AppController],
     providers: [AppService],
 })
--- backend/src/experience/experience.module.ts
+++ backend/src/experience/experience.module.ts
@@ -4,14 +4,16 @@ import {
     Experience,
     ExperienceSchema
 } from "@/experience/schemas/experience.schema";
+import {ExperienceService} from "@/experience/experience.service";
+import {ExperienceController} from "@/experience/experience.controller";
 
 @Module({
     imports: [MongooseModule.forFeature([{
         name: Experience.name,
         schema: ExperienceSchema
     }])],
-    providers: [],
-    controllers: []
+    providers: [ExperienceService],
+    controllers: [ExperienceController]
 })
 
 export class ExperienceModule {}
