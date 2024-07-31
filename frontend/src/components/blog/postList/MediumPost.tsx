989ea3b9f0773ed873be1e83cd63394d2c40c440
Maximilian <mpinderwhite@gmail.com>
2024-07-31 17:14:05 +0200

    [~] used new utils

--- frontend/src/components/blog/postList/MediumPost.tsx
+++ frontend/src/components/blog/postList/MediumPost.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useSelectedPost } from "@/providers/SelectedPostProvider";
 import { IPost } from "@common/types/IPost";
-import {twMerge} from "tailwind-merge";
+import {cn} from "@/lib/utils";
 
 interface MediumPostProps {
     post: IPost;
@@ -11,7 +11,7 @@ const MediumPost: React.FC<MediumPostProps> = ({ post }) => {
     const { setSelectedPost, selectedPost } = useSelectedPost();
     return (
         // @ts-ignore
-        <Card onClick={() => setSelectedPost(post)} className={twMerge("cursor-pointer mb-4 hover:bg-gray-100 transition-all", selectedPost?._id === post._id && "bg-gray-100")}>
+        <Card onClick={() => setSelectedPost(post)} className={cn("cursor-pointer mb-4 hover:bg-gray-100 transition-all", selectedPost?._id === post._id && "bg-gray-100")}>
             <CardHeader>
                 <CardTitle>{post.title}</CardTitle>
             </CardHeader>
