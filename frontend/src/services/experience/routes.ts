8dcf4f1e22e989e69c1e8068b6c09a80840b66e1
Maximilian <mpinderwhite@gmail.com>
2024-07-31 16:43:01 +0200

    [~] corrected form data appending

--- frontend/src/services/experience/routes.ts
+++ frontend/src/services/experience/routes.ts
@@ -1,19 +1,25 @@
 import {API_URL} from "@common/utils/constants";
 import axios from "@/providers/axios";
 import {ICreateExperienceDto} from "@common/dto/ICreateExperienceDto";
+
 const EXPERIENCE_URL = `${API_URL}/experience`
 
 export const getExperiences = async () => axios.get(EXPERIENCE_URL)
 
-export const addExperience = async (experience : ICreateExperienceDto) => {
+export const addExperience = async (experience: ICreateExperienceDto) => {
     const formData = new FormData();
-    formData.append('title', experience.title);
+    formData.append('company', experience.company);
     formData.append('content', experience.description);
+    formData.append('startDate', experience.startDate.toLocaleString())
+    if (experience.endDate) {
+        formData.append('endDate', experience.endDate.toLocaleString())
+    }
     if (experience.image) {
         formData.append('image', experience.image);
     }
 
-    const response = await axios.post(EXPERIENCE_URL, formData , {
+
+    const response = await axios.post(EXPERIENCE_URL, formData, {
         headers: {
             'Content-Type': 'multipart/form-data',
         }
