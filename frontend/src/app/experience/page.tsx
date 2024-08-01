6db0e19c8f2b8c67a8f7d11ba5e4801f15d756fa
Maximilian <mpinderwhite@gmail.com>
2024-08-01 23:43:41 +0200

    [+] added new button to page

--- frontend/src/app/experience/page.tsx
+++ frontend/src/app/experience/page.tsx
@@ -1,8 +1,10 @@
 import ExperienceList from "@/components/experience/ExperienceList";
+import ExperienceButton from "@/components/experience/ExperienceButton";
 
 function Page() {
     return (
         <main className={'abs-center'}>
+            <ExperienceButton/>
             <ExperienceList/>
         </main>
     );
