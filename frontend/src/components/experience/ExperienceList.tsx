7dc3a3c31aba58c15317b1876c6082bd87d6d089
Maximilian <mpinderwhite@gmail.com>
2024-08-06 12:54:16 +0200

    [+] base design for ExperienceList.tsx

--- frontend/src/components/experience/ExperienceList.tsx
+++ frontend/src/components/experience/ExperienceList.tsx
@@ -1,40 +1,53 @@
-"use client"
+"use client";
 import {IExperience} from "@common/types/IExperience";
-import Image from 'next/image'
+import Image from "next/image";
 import {useExperienceQuery} from "@/services/experience";
 import {cn} from "@/lib/utils";
 
-
 function ExperienceList() {
-    const {data, isLoading} = useExperienceQuery()
+    const query = useExperienceQuery();
 
-    if (isLoading){
-        return <h1>Loading...</h1>
+    if (query.isLoading) {
+        return <h1>Loading...</h1>;
     }
-    if (data) {
-        return (
-            <ul className={'flex flex-col'}>
-                {data.map((experience: IExperience, index: number) => {
-                    return (
-                        <li key={index}
-                            className={cn('flex w-full h-full', index % 2 == 0 && 'flex-row-reverse')}>
-                            <div className={'w-1/3'}>
-                                <h1 className={'text-xl'}>{experience.company}</h1>
-                                {experience.startDate && experience.endDate &&
-                                    <h3 className={'font-bold text-lg'}>{experience.startDate.toLocaleString()} - {experience.endDate.toLocaleString()}</h3>}
-                                <p className={'mt-6'}>{experience.description}</p>
-                            </div>
-                            <div className={'w-1/3 h-full'}>
-                                <Image className={'object-cover'} fill={true}
-                                       src={experience.image}
-                                       alt={`${experience.company}_image`}/>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-        )
+
+    if (query.error) {
+        return <h1>Failed to load experiences</h1>;
     }
+
+    return (
+        <ul className="abs-center flex flex-col space-y-6 p-3 w-2/3">
+            {query.data && query.data.map((experience: IExperience, index: number) => {
+                const startDate = experience.startDate ? new Date(experience.startDate).toLocaleDateString() : null;
+                const endDate = experience.endDate ? new Date(experience.endDate).toLocaleDateString() : "Present";
+
+                return (
+                    <li
+                        key={index}
+                        className={cn("flex w-full h-full", index % 2 === 0 ? "flex-row-reverse" : "flex-row")}
+                    >
+                        <div className="w-1/3 p-4">
+                            <h1 className="text-xl font-bold">{experience.company}</h1>
+                            {startDate && (
+                                <h3 className="font-bold text-lg">
+                                    {startDate} - {endDate}
+                                </h3>
+                            )}
+                            <p className="mt-6">{experience.description}</p>
+                        </div>
+                        <div className="w-full h-[200px] relative">
+                            <Image
+                                className="object-cover"
+                                fill={true}
+                                src={experience.image}
+                                alt={`${experience.company} image`}
+                            />
+                        </div>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }
 
-export default ExperienceList
+export default ExperienceList;
