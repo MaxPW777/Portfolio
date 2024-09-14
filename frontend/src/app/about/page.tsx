09ee451b2b73f660123a51f30435cb9b5f2f62fc
Maximilian PINDER-WHITE <mpinderwhite@gmail.com>
2024-09-14 22:39:32 +0200

    [+] added new pages

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+function AboutComponent() {
+    return null;
+}
+
+export function page() {
+    return (
+        <main className="flex h-screen min-h-screen items-center justify-center">
+            <AboutComponent/>
+        </main>
+    );
+}
---
+++ frontend/src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+function ProjectsComponent() {
+    return null;
+}
+
+export function page() {
+    return (
+        <main className="flex h-screen min-h-screen items-center justify-center">
+            <ProjectsComponent />
+        </main>
+    );
+}
