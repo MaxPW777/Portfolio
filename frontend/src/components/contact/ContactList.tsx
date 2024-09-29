69fe56bc24f39cdda5b6e340bd7481cfe5d10e07
Maximilian <mpinderwhite@gmail.com>
2024-09-29 21:32:23 +0200

    [~] changed text

--- frontend/src/components/contact/ContactList.tsx
+++ frontend/src/components/contact/ContactList.tsx
@@ -1,5 +1,4 @@
 "use client"
-import {useGetContactsQuery} from "@/services/contact";
 import {IContactRequest} from "@common/types/IContactRequest";
 import {useAuth} from "@/providers/auth-context";
 import Link from "next/link";
@@ -12,11 +11,15 @@ function ContactList({contactData}: IContactListProps) {
     const {isAuthenticated} = useAuth()
 
     if (!isAuthenticated) {
-        return <div>
+        return <div className={'flex flex-col w-[200px] gap-10'}>
             <p>
-                afin de voir les messages et decouvrir le site veuillez vous connecter sur le compte administateur
-                <Link href={'/login'}>CLIQUEZ ICI</Link>
-                avec le nom d&#39;utilisateur: <strong>max</strong> et le mot de passe: <strong>password</strong>
+                afin de voir les messages et decouvrir le site veuillez vous
+                connecter sur le compte administateur
+            </p>
+            <Link className={'text-red-600 text-xl'} href={'/login'}>CLIQUEZ ICI</Link>
+            <p className={'flex flex-col gap-5'}>
+                avec le nom d&#39;utilisateur: <strong>max</strong> et le mot de
+                passe: <strong>password</strong>
             </p>
         </div>
     }
@@ -25,10 +28,11 @@ function ContactList({contactData}: IContactListProps) {
         <div className={'contact overflow-y-auto h-[600px] w-[600px] p-4'}>
             <ul className={'flex flex-col gap-y-2.5'}>
                 {contactData && contactData.map((contact: IContactRequest, index: number) => (
-                    <li className={'border rounded p-3 flex flex-col gap-y-2.5'} key={index}>
+                    <li className={'border rounded p-3 flex flex-col gap-y-2.5'}
+                        key={index}>
                         <div className={'flex justify-between'}>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
                         </div>
                         <p className={'w-full'}>{contact.message}</p>
                     </li>
